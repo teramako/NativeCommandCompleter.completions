5966123f0cff7066750cd354e867a811bf46b73b
teramako <teramako@gmail.com>
2026-04-28 17:18:42 +0900

    ♻️ Fix code format of argument compeleter: `groupadd`

--- completions/groupadd.ps1
+++ completions/groupadd.ps1
@@ -22,14 +22,14 @@ foreach ($key in $localizedMessages.Keys) { $msg[$key] = $localizedMessages[$key
 
 Register-NativeCompleter -Name groupadd -Description $msg.groupadd -Parameters @(
     New-ParamCompleter -ShortName f -LongName force -Description $msg.force
-    New-ParamCompleter -ShortName g -LongName gid -Description $msg.gid -VariableName 'GID'
+    New-ParamCompleter -ShortName g -LongName gid -Description $msg.gid -Arguments @{ Name = 'GID' }
     New-ParamCompleter -ShortName h -LongName help -Description $msg.help
-    New-ParamCompleter -ShortName K -LongName key -Description $msg.key -VariableName 'KEY=VALUE'
+    New-ParamCompleter -ShortName K -LongName key -Description $msg.key -Arguments @{ Name = 'KEY=VALUE' }
     New-ParamCompleter -ShortName o -LongName non-unique -Description $msg.non_unique
-    New-ParamCompleter -ShortName p -LongName password -Description $msg.password -VariableName 'PASSWORD'
+    New-ParamCompleter -ShortName p -LongName password -Description $msg.password -Arguments @{ Name = 'PASSWORD' }
     New-ParamCompleter -ShortName r -LongName system -Description $msg.system
-    New-ParamCompleter -ShortName R -LongName root -Description $msg.root -ArgumentType Directory -VariableName 'CHROOT_DIR'
-    New-ParamCompleter -ShortName P -LongName prefix -Description $msg.prefix -ArgumentType Directory -VariableName 'PREFIX_DIR'
-    New-ParamCompleter -ShortName U -LongName users -Description $msg.users -ArgumentType List -VariableName 'USER'
+    New-ParamCompleter -ShortName R -LongName root -Description $msg.root -Arguments @{ Name = 'CHROOT_DIR'; Type = 'Directory' }
+    New-ParamCompleter -ShortName P -LongName prefix -Description $msg.prefix -Arguments @{ Name = 'PREFIX_DIR'; Type = 'Directory' }
+    New-ParamCompleter -ShortName U -LongName users -Description $msg.users -Arguments @{ Name = 'USER'; List = $true }
     New-ParamCompleter -LongName extrausers -Description $msg.extrausers
 ) -NoFileCompletions
