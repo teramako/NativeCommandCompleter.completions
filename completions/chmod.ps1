f91c07a11cf48311ca45b40c0c50dbfd6cea3cd6
teramako <teramako@gmail.com>
2026-05-04 01:17:13 +0900

    👍 Add MODE(permission) completer

--- completions/chmod.ps1
+++ completions/chmod.ps1
@@ -35,7 +35,36 @@ Register-NativeCompleter -Name chmod -Description $msg.chmod -Parameters @(
             [MT.Comp.Helper]::CompleteFilename($this, $true, $true);
             return;
         }
-        return $null;
+        $word = $this.WordToComplete;
+        switch -Regex ($word)
+        {
+            '^[-+=]?[0-7]+' {
+                if ($word.Length -ge 3) { return $null }
+                0 .. 7 | ForEach-Object { "${word}$_" }
+            }
+            '^[ugoa]?[ugo]*$' {
+                "${word}+`tOperator: Add"
+                "${word}-`tOperator: Remove"
+                "${word}=`tOperator: Overwrite"
+                $t = $Matches[0];
+                if (-not $t.Contains("a")) {
+                    if (-not $Matches[0].Contains("u")) { "${word}u`tTarget: User" }
+                    if (-not $Matches[0].Contains("g")) { "${word}g`tTarget: Group" }
+                    if (-not $Matches[0].Contains("o")) { "${word}o`tTarget: Other" }
+                }
+            }
+            '[ugoa]?[ugo]*[-+=]([rwxXst]*)' {
+                $p = $Matches[1];
+                if (-not $p.Contains("r")) { "${word}r`tPerm: read" }
+                if (-not $p.Contains("w")) { "${word}w`tPerm: write" }
+                if (-not $p -contains "x") {
+                    "${word}x`tPerm: execute/search"
+                    "${word}X`tPerm: execute/search only if directory or already executable"
+                }
+                if (-not $p.Contains("s")) { "${word}s`tPerm: set uid/gid" }
+                if (-not $p.Contains("t")) { "${word}s`tPerm: set sticky bit" }
+            }
+        }
     }
 }, @{
     Name = 'FILE';
--- completions/mkdir.ps1
+++ completions/mkdir.ps1
@@ -18,7 +18,41 @@ mkdir --version 2>&1 | Out-Null
 if ($LASTEXITCODE -eq 0) # GNU mkdir
 {
     Register-NativeCompleter -Name mkdir -Parameters @(
-        New-ParamCompleter -ShortName m -LongName mode -Description $msg.mode -Arguments @{ Name = 'MODE' }
+        New-ParamCompleter -ShortName m -LongName mode -Description $msg.mode -Arguments @{
+            Name = 'MODE'
+            Script = {
+                $word = $this.WordToComplete;
+                switch -Regex ($word)
+                {
+                    '^[-+=]?[0-7]+' {
+                        if ($word.Length -ge 3) { return $null }
+                        0 .. 7 | ForEach-Object { "${word}$_" }
+                    }
+                    '^[ugoa]?[ugo]*$' {
+                        "${word}+`tOperator: Add"
+                        "${word}-`tOperator: Remove"
+                        "${word}=`tOperator: Overwrite"
+                        $t = $Matches[0];
+                        if (-not $t.Contains("a")) {
+                            if (-not $Matches[0].Contains("u")) { "${word}u`tTarget: User" }
+                            if (-not $Matches[0].Contains("g")) { "${word}g`tTarget: Group" }
+                            if (-not $Matches[0].Contains("o")) { "${word}o`tTarget: Other" }
+                        }
+                    }
+                    '[ugoa]?[ugo]*[-+=]([rwxXst]*)' {
+                        $p = $Matches[1];
+                        if (-not $p.Contains("r")) { "${word}r`tPerm: read" }
+                        if (-not $p.Contains("w")) { "${word}w`tPerm: write" }
+                        if (-not $p -contains "x") {
+                            "${word}x`tPerm: execute/search"
+                            "${word}X`tPerm: execute/search only if directory or already executable"
+                        }
+                        if (-not $p.Contains("s")) { "${word}s`tPerm: set uid/gid" }
+                        if (-not $p.Contains("t")) { "${word}s`tPerm: set sticky bit" }
+                    }
+                }
+            }
+        }
         New-ParamCompleter -ShortName p -LongName parents -Description $msg.parents
         New-ParamCompleter -ShortName v -LongName verbose -Description $msg.verbose
         New-ParamCompleter -LongName version -Description $msg.version
